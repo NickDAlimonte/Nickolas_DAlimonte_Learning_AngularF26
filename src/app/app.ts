6e4877db41fc076befcc9d6ba7d5f6753b12b4f5
Nick D'Alimonte <nickolas.dalimonte@gmail.com>
2026-09-15 15:52:35 -0400

    Imported and created 6 instances of the character models under app.ts and included them in an array.
    Added a game property to the character model.
    
    Began the HTML layout for my character list. Included a loop for the character array, and conditionally removing elements from the list based on whether or not the information exists in the character model.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,74 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Character } from './shared/models/character';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('Nickolas_DAlimonte_learning_AngularF26');
-  testVar1: string = "This is the first test variable";
+  testVar1: string = 'This is the first test variable';
   testVar2: number = 300;
+
+  characterList: Character[] = [
+    {
+      name: 'Vaelysong',
+      levelCap: true,
+      class: "Warrior",
+      level: 90,
+      race: 'Void Elf',
+      gearQuality: 286,
+      game: "World of Warcraft",
+    },
+
+    {
+      name: 'Aelithiria',
+      race: "Alvarin",
+      class: "Dex Footie",
+      gearQuality: "Pansar Carapace",
+      game: "Mortal Online II",
+    },
+
+    {
+      name: "Keliza",
+      race: "Alvarin",
+      class: "Hybrid",
+      gearQuality: "Plate",
+      game: "Mortal Online 2",
+    },
+
+    {
+      name: "Keliza",
+      race: "Draenei",
+      class: "Paladin",
+      levelCap: false,
+      level: 81,
+      gearQuality: 90,
+      game: "World of Warcraft",
+    },
+
+    {
+      name: "Aelithiria",
+      race: "Void Elf",
+      class: "Mage",
+      levelCap: true,
+      level: 90,
+      gearQuality: 248,
+      game: "World of Warcraft",
+    },
+    {
+      name: "Kitiza",
+      race: "Void Elf",
+      class: "Priest",
+      levelCap: false,
+      level: 86,
+      gearQuality: 126,
+      game: "World of Warcraft",
+    }
+
+
+  ];
 }
--- src/app/shared/models/character.ts
+++ src/app/shared/models/character.ts
@@ -2,8 +2,9 @@ export interface Character {
   name: string;
   class: string;
   race: string;
-  level: number;
-  levelCap: boolean;
-  gearQuality?: string | number;
+  level?: number;
+  levelCap?: boolean;
+  gearQuality: string | number;
+  game: string;
 
 }
